759094d040d88fe90a2f247fcf5100ce5dc46453
hibo <hiboabdallah402@gmail.com>
2026-02-19 22:34:18 +0300

    done

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    about: "A brief description of project 1",
+    technologies: ["React", "JavaScript"],
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    about: "A brief description of project 2",
+    technologies: ["React", "CSS", "Node.js"],
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    about: "A brief description of project 3",
+    technologies: ["JavaScript", "HTML", "CSS"],
+  },
+];
+
+export default projects;
